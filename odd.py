d1a2549d96a352a0e1a902ab8b668189761ceadc
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-12 20:51:58 -0700

    odd

--- odd.py
+++ odd.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 from os import getcwd
 import random
-import
+import time
 
 odds = [ 1,  3,  5,  7,  9, 11, 13, 15, 17, 19,
         21, 23, 25, 27, 29, 31, 33, 35, 37, 39,
