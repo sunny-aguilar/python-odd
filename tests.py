c4ad75ac381523cd6cfcd9a260dfd6b2a1c70fe4
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-12 22:23:36 -0700

    odd

--- tests.py
+++ tests.py
@@ -1,1 +1,14 @@
-range(1, 5)
+word = "bottles"
+for beer_num in range(99, 0, -1):
+    print(beer_num, word, "of beer on the wall.")
+    print(beer_num, word, "of beer.")
+    print("Take one down.")
+    print("Pass it around.")
+    if beer_num == 1:
+        ("No more bottles of beer on the wall.")
+    else:
+        new_num = beer_num - 1
+        if new_num == 1:
+            word = "bottle"
+        print(new_num, word, "of beer on the wall.")
+    print("")
