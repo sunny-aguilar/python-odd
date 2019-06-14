f75d15caa3c264c41f7f0dcbca221ae3600bfda1
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-13 22:51:54 -0700

    odd

--- vowels.py
+++ vowels.py
@@ -1,14 +1,11 @@
-# vowels = ['a', 'e', 'i', 'o', 'u']
-# word = input("Provide a word to search for vowels: ")
-# found = []
+vowels = ['a', 'e', 'i', 'o', 'u']
+word = input("Provide a word to search for vowels: ")
+found = []
 
-# for letter in word:
-#     if letter in vowels:
-#         if letter not in found:
-#             found.append(letter)
+for letter in word:
+    if letter in vowels:
+        if letter not in found:
+            found.append(letter)
 
-# for vowel in found:
-#     print(vowel)
-
-name = input("Type your name: ")
-print("Hell " + name)
+for vowel in found:
+    print(vowel)
