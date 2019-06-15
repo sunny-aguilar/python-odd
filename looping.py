bca230ae5c4ae17dd623efac654c54cc69889dd8
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-14 21:04:00 -0700

    loop

--- looping.py
+++ looping.py
@@ -12,7 +12,11 @@
 
 list_one = [0, 1, 2, 3, 4, 5]
 list_two = [5, 6, 7, 8, 9]
+
+# add two lists together
 list_one.extend(list_two)
 print(list_one)
+
+# insert(where, what)
 list_one.insert(0, -1)
 print(list_one)
