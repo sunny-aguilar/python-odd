b4a493b9eaed03c5e6c2b7e02b8d906ac8984b50
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-14 20:35:10 -0700

    loop

--- looping.py
+++ looping.py
@@ -1,8 +1,13 @@
-mylist = [1, 2, 3, 4, 5, 6]
-print(mylist)
-mylist.remove(1)
-print(mylist)
+# mylist = [1, 2, 3, 4, 5, 6]
+# print(mylist)
+# mylist.remove(1)
+# print(mylist)
+#
+# x = mylist.pop(2)
+# print(mylist)
+# print(x)
 
-x = mylist.pop(2)
-print(mylist)
-print(x)
+list_one = [1, 2, 3, 4, 5, 6]
+list_two = [7, 8, 9]
+list_one.extend(list_two)
+print(list_one)
