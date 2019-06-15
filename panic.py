0897d93fcfa70400e8f1bafac61a874b89bcab27
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-14 21:17:01 -0700

    loop

--- panic.py
+++ panic.py
@@ -3,8 +3,8 @@ plist = list(phrase)
 print(phrase)
 print(plist)
 # ['D', 'o', 'n', "'", 't', ' ', 'p', 'a', 'n', 'i', 'c', '!']
-# convert to "on tap
+# convert to "on tap"
 
-
+newPhrase = []
 for a in plist:
     print(a)
