09bae631ddfeb221c7af0e198433782d2165e765
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-17 19:28:19 -0700

    practice

--- panic.py
+++ panic.py
@@ -10,13 +10,13 @@
 # ['D', 'o', 'n', "'", 't', ' ', 'p', 'a', 'n', 'i', 'c', '!']
 # convert to "on tap"
 
-for i in range(4):
-    plist.pop()
-plist.pop(0)
-plist.remove("'")
-plist.extend([plist.pop(), plist.pop()])
-plist.insert(2, plist.pop(3))
-
-new_phrase = ''.join(plist)
-print(plist)
-print(new_phrase)
+# for i in range(4):
+#     plist.pop()
+# plist.pop(0)
+# plist.remove("'")
+# plist.extend([plist.pop(), plist.pop()])
+# plist.insert(2, plist.pop(3))
+#
+# new_phrase = ''.join(plist)
+# print(plist)
+# print(new_phrase)
