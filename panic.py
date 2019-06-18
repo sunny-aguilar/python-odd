62e2c932d0f8f9bf4da7a8d966a4d4def5a4ee9d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-17 20:24:58 -0700

    practice

--- panic.py
+++ panic.py
@@ -2,26 +2,26 @@
 # Date:             June 17, 2019
 # Description:      Python program
 
-# phrase = "Don't panic!"
-# plist = list(phrase)
-# print(phrase)
-# print(plist)
+phrase = "Don't panic!"
+plist = list(phrase)
+print(phrase)
+print(plist)
 # what prints
 # ['D', 'o', 'n', "'", 't', ' ', 'p', 'a', 'n', 'i', 'c', '!']
 # convert to "on tap"
 
-# for i in range(4):
-#     plist.pop()
-# plist.pop(0)
-# plist.remove("'")
-# plist.extend([plist.pop(), plist.pop()])
-# plist.insert(2, plist.pop(3))
-#
-# new_phrase = ''.join(plist)
-# print(plist)
-# print(new_phrase)
-myName = "Sandro Aguilar lives in Roseville"
-myList = list(myName)
+for i in range(4):
+    plist.pop()
+plist.pop(0)
+plist.remove("'")
+plist.extend([plist.pop(), plist.pop()])
+plist.insert(2, plist.pop(3))
 
-print(''.join(myList[::2]))
-print(''.join(myList[13:6:-1]))
+new_phrase = ''.join(plist)
+print(plist)
+print(new_phrase)
+# myName = "Sandro Aguilar lives in Roseville"
+# myList = list(myName)
+#
+# print(''.join(myList[::2]))
+# print(''.join(myList[13:6:-1]))
