4aec116231ab6c930c458bd14adb1d7d74e6aa93
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-19 21:49:46 -0700

    python

--- dictionaries.py
+++ dictionaries.py
@@ -3,5 +3,12 @@ alien_0 = {
     'points': 5
 }
 
+# print single elements from dictionary
 print(alien_0['color'])
 print(alien_0['points'])
+print(alien_0)
+
+# add additional values to dictionary
+alien_0['eyes'] = 4
+alien_0['name'] = 'Bob'
+print(alien_0)
