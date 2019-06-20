dd879a0f10520163a912b37c2e9dc31939747540
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-20 08:14:47 -0700

    python

--- dictionaries.py
+++ dictionaries.py
@@ -23,7 +23,8 @@ del alien['speed']
 print(alien)
 
 points = alien.get('points', "no points have been set")
-print(points)
+print(points, '\n')
+
 
 user = {
     'username': "vluedevil",
@@ -31,5 +32,6 @@ user = {
     'last': 'aguilar',
 }
 
-
+for key in user.items():
+    print(key)
 
