25754ccaae14933287272d1e772a912bda2336c2
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-20 15:14:46 -0700

    python

--- userinput.py
+++ userinput.py
@@ -5,9 +5,9 @@
 # print(f"Hello {name}!")
 
 # int() for converting string to integer
-age = input("What is your age? ")
-age = int(age)
-if age > 25:
-    print("welcome adult!")
+# age = input("What is your age? ")
+# age = int(age)
+# if age > 25:
+#     print("welcome adult!")
 
-
+# while loops
