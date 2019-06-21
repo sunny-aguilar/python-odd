dcc08d6f41d9f308887a99a75602b3a918b34500
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-20 21:29:21 -0700

    python

--- functions.py
+++ functions.py
@@ -20,6 +20,20 @@ def sayname(first, last):
 name = sayname("sandro", "aguilar")
 print(name)
 
+def build_person(first_name, last_name, age=None):
+    person = {'first': first_name, 'last': last_name}
+    if age:
+        person['age'] = age
+    return person
 
+programmer = build_person("sandro", "aguilar", 37)
+print(programmer)
+print('\n')
+
+def multiple_values(*values):
+    print(values)
+
+multiple_values('honda', 'ford', 'BMW')
+multiple_values('chevy')
 
 
---
+++ modules.py
@@ -0,0 +1,9 @@
+# import a module
+import my_module
+
+my_module.say_hello()
+
+
+
+
+
