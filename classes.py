0a05c7bbcfbf44adca8f957e479dd7e347b9af1c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-21 15:51:23 -0700

    python

--- classes.py
+++ classes.py
@@ -37,10 +37,12 @@ print('\n')
 
 
 """ import car class """
-from car import ElectricCar
+#from car import ElectricCar
+
+import car
 
 
-my_tesla = ElectricCar('tesla', 'model s', 2019)
+my_tesla = car.ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.descriptive_name())
 my_tesla.battery.describe_battery()
 my_tesla.fill_tank()
