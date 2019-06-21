c90137b350a5fef5b97dbb9e723710d5ae2f6e78
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-21 12:17:01 -0700

    python

--- classes.py
+++ classes.py
@@ -35,33 +35,7 @@ my_dog.bark()
 my_dog.roll_over()
 print('\n')
 
-# car super class
-class Car:
-    def __init__(self, make, model, year):
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer = 0
 
-    def setOdometer(self, miles):
-        if miles > self.odometer:
-            self.odometer = miles
-        else:
-            print("error setting mileage")
-
-    def getMiles(self):
-        return self.odometer
-
-    def carType(self):
-        message = f"{self.make}, {self.model}, {self.odometer} miles"
-        return message
-
-    def descriptive_name(self):
-        name = f"{self.make} {self.model}"
-        return name.title()
-
-    def fill_tank(self):
-        print("Filling gas tank")
 
 # battery class
 class Battery():
