b4333a078116388e246166f15cf793d9c7437efa
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-21 11:47:33 -0700

    python

--- classes.py
+++ classes.py
@@ -66,6 +66,7 @@ class ElectricCar(Car):
     def __init__(self, make, model, year):
         super().__init__(make, model, year)
 
+
 my_tesla = ElectricCar('tesla', 'model s', 2019)
 print(my_tesla.descriptive_name())
 
