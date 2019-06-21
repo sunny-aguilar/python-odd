55a5d7f8bc7317ab8bc2e70a5a78fbd1560f7108
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-21 12:19:22 -0700

    python

--- classes.py
+++ classes.py
@@ -39,24 +39,7 @@ print('\n')
 """ import car class """
 from car import Car
 
-# battery class
-class Battery():
-    def __init__(self):
-        self.battery = 75
 
-    def describe_battery(self):
-        """ print a statement describing battery """
-        print(f"This car has a {self.battery}-kWh battery.")
-
-# child class has a battery class
-class ElectricCar(Car):
-    """ electric version of car class """
-    def __init__(self, make, model, year):
-        super().__init__(make, model, year)
-        self.battery = Battery()
-
-    def fill_tank(self):
-        print("Electric cars don't have gas tanks!")
 
 
 my_tesla = ElectricCar('tesla', 'model s', 2019)
