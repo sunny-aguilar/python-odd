9478b12cba840624030a7421bb614ae913da8d7c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-06-23 12:03:31 -0700

    lambda

--- unitTest.py
+++ unitTest.py
@@ -1,12 +1,31 @@
 
 import unittest
-from name_function import get_formatted_name
+
+def get_formatted_name(first, last):
+    full_name = f"{first} {last}"
+    return full_name.title()
+
+
+print("Enter 'q' at any time to quit.")
+while True:
+    first = input("\nPlease give me a first name: ")
+    if first == 'q':
+        break
+    last = input("Please give me a last name: ")
+    if last == 'q':
+        break
+
+    formatted_name = get_formatted_name(first, last)
+    print(f"\tNeatly formatted name: {formatted_name}.")
+
+
 
 class NamesTestCase(unittest.TestCase):
     def test_first_last_name(self):
         formatted_name = get_formatted_name('sandro', 'aguilar')
         self.assertEqual(formatted_name, 'Sandro Aguilar')
 
-
+if __name__ == '__main__':
+    unittest.main()
 
 
