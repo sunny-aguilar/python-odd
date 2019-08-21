e369fe22b778d6b62d434339cbbbfab121610ae1
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-20 22:09:20 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -1,18 +1,18 @@
 
-name = 'sandro'
-age = 37
-print(name, end=' ')
-print(age)
-
-print(1,2,3,4,5,sep='*')
-
-print(format(232.1482224, '9,.2f'))
-
-if 'Sandro' is 'Sandro':
-    print('equivalent')
-
-if 5 > 2:
-    print('yay!')
+# name = 'sandro'
+# age = 37
+# print(name, end=' ')
+# print(age)
+#
+# print(1,2,3,4,5,sep='*')
+#
+# print(format(232.1482224, '9,.2f'))
+#
+# if 'Sandro' is 'Sandro':
+#     print('equivalent')
+#
+# if 5 > 2:
+#     print('yay!')
 
 
 # #x = input('Enter a number for x: ')
