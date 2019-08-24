48bcd51e300afb3560569c649925d7b84b08826a
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-24 10:55:17 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -46,8 +46,8 @@
 #     print('that is correct!')
 #
 
-for row in range(8):
-    for col in range(6):
-        print('*', end='')
-    print()
+# for row in range(8):
+#     for col in range(6):
+#         print('*', end='')
+#     print()
 
