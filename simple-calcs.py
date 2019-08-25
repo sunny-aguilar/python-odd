682f70500e26a996c801e98d3b29c83ae62a1308
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-25 14:51:28 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -104,7 +104,7 @@
 # print(mylistcopy)
 
 
-matrix = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 8]]
+matrix = [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10]]
 for r in range(3):
     for c in range(3):
         print(matrix[r][c])
