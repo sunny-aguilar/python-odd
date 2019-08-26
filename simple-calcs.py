99909ea05f889c85a8dfe07dacd78387b8d2502d
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-25 17:25:05 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -129,7 +129,7 @@
 #
 # print(newName)
 
-myName = 'Sandro Aguilar'
-for ch in myName:
-    if ch == 'a' or ch == 'r':
-        print('Character found')
+# myName = 'Sandro Aguilar'
+# for ch in myName:
+#     if ch == 'a' or ch == 'r':
+#         print('Character found')
