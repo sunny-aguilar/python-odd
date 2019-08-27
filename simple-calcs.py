493b3dc2c5a0a713ac74b331411c9371a1f80246
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 19:13:13 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -191,4 +191,21 @@ phonebook = {
 # myClass.printName()
 
 
+import random
+
+class Coin:
+    def __init__(self):
+        self.sideup = 'Heads'
+
+    def toss(self):
+        if random.randint(0, 1) == 0:
+            self.sideup = 'Heads'
+        else:
+            self.sideup = 'Tails'
+
+    def get_sideup(self):
+        return self.sideup
+
+
+def main():
 
