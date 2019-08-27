f12a2488a4a305456a331fd51db364516dad445e
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 20:11:26 -0700

    py

---
+++ module.py
@@ -0,0 +1,16 @@
+
+
+
+def main():
+    my_coin = Coin()
+    print('This side is up: ', my_coin.get_sideup())
+    print('I am tossing the coin ...')
+    my_coin.toss()
+    my_coin.__sideup = 'Heads'      # no longer works! its private now bitches
+
+    print('I am going to toss the coin ten times:')
+    for count in range(10):
+        my_coin.toss()
+        print(my_coin.get_sideup())
+
+main()
