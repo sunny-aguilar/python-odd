c25584703c6ac321ff6c5634bee4797473a5de5c
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 19:15:58 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -210,5 +210,9 @@ class Coin:
 def main():
     my_coin = Coin()
     print('This side is up: ', my_coin.get_sideup())
+    print('I am tossing the coin ...')
+    my_coin.toss()
+    print('This side is up:', my_coin.toss())
 
 
+# main()
