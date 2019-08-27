5869ba1dcca3b0aa0e92556a255d41656fb7a4fd
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 20:10:56 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -207,16 +207,4 @@ class Coin:
         return self.__sideup
 
 
-def main():
-    my_coin = Coin()
-    print('This side is up: ', my_coin.get_sideup())
-    print('I am tossing the coin ...')
-    my_coin.toss()
-    my_coin.__sideup = 'Heads'      # no longer works! its private now bitches
 
-    print('I am going to toss the coin ten times:')
-    for count in range(10):
-        my_coin.toss()
-        print(my_coin.get_sideup())
-
-main()
