171d8e28e7f6b833b7f3429ef3bd2ba0c9ac6c24
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 19:23:11 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -212,7 +212,8 @@ def main():
     print('This side is up: ', my_coin.get_sideup())
     print('I am tossing the coin ...')
     my_coin.toss()
-    print('This side is up:', my_coin.toss())
+    my_coin.sideup = 'Heads'
+    print('This side is up:', my_coin.get_sideup())
 
 
 main()
