6a7b622210875d33d84526d9b15f86e059ba7420
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 20:44:42 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -200,8 +200,10 @@ import module
 # main program
 def main():
     my_coin = module.Coin()
+
     print('This side is up: ', my_coin.get_sideup())
     print('I am tossing the coin ...')
+
     my_coin.toss()
     my_coin.__sideup = 'Heads'      # no longer works! its private now bitches
 
@@ -209,5 +211,7 @@ def main():
     for count in range(10):
         my_coin.toss()
         print(my_coin.get_sideup())
+
+    print(my_coin.__str__())
 
 main()
