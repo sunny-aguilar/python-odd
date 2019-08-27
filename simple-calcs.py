5732351784db43d0f732a3c753ff5dff92f7ce50
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-08-26 20:02:17 -0700

    py

--- simple-calcs.py
+++ simple-calcs.py
@@ -214,7 +214,9 @@ def main():
     my_coin.toss()
     my_coin.__sideup = 'Heads'      # no longer works! its private now bitches
 
-    print('This side is up:', my_coin.get_sideup())
+    print('I am going to toss the coin ten times:')
+    for count in range(10):
+        my_coin.toss()
 
 
 main()
